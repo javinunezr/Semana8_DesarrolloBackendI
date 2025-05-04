39790350b8297bb66bd1264993ad1d8c429e39b3
LeoOlivaresD <cspraword@gmail.com>
2025-05-04 15:02:32 -0400

    Refactor de productos

--- src/main/java/com/duoc/RopaStoreValidation/models/Productos.java
+++ src/main/java/com/duoc/RopaStoreValidation/models/Productos.java
@@ -1,7 +1,9 @@
 package com.duoc.RopaStoreValidation.models;
 
 import jakarta.persistence.*;
-
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,12 +18,12 @@ public class Productos {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "profile_info_seq_productos")
-    @SequenceGenerator(name="profile_info_seq_productos",sequenceName = "profile_info_seq_productos",allocationSize = 1,initialValue = 10000)
+    @SequenceGenerator(name = "profile_info_seq_productos", sequenceName = "profile_info_seq_productos", allocationSize = 1, initialValue = 10000)
     private Long idProducto;
 
     @NotNull
     @Column(name = "nombreProducto", nullable = false) // Obligatorio
-    @Size(min=1,max=40)
+    @Size(min = 1, max = 40)
     private String nombreProducto;
 
     @NotNull
@@ -31,7 +33,7 @@ public class Productos {
 
     @NotNull
     @Column(name = "categoriaProducto", nullable = false) // Obligatorio
-    @Size(min=1,max=25)
+    @Size(min = 1, max = 25)
     private String categoriaProducto;
 
     @NotNull
@@ -41,6 +43,6 @@ public class Productos {
 
     @NotNull
     @Column(name = "sucursalId", nullable = false) // Obligatorio
-    @Size(min=1,max=15)
+    @Size(min = 1, max = 15)
     private Long sucursalId;
 }
