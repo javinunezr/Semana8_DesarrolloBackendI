611177875a9a57dbe6008600a86ca52efd33bcad
LeoOlivaresD <cspraword@gmail.com>
2025-05-04 15:03:18 -0400

    Refactor cliente service

--- src/main/java/com/duoc/RopaStoreValidation/services/ClientesService.java
+++ src/main/java/com/duoc/RopaStoreValidation/services/ClientesService.java
@@ -2,13 +2,20 @@ package com.duoc.RopaStoreValidation.services;
 
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
 import com.duoc.RopaStoreValidation.models.Clientes;
 
 @Service
 public interface ClientesService {
-    List<Clientes> getAllClientes()
-    Clientes createCliente(Clientes clientes);    
+    List<Clientes> getAllClientes();
+
+    Clientes createCliente(Clientes clientes);
+
     Clientes updateCliente(Long id, Clientes clientes);
-    Optional<Clientes> getClientesById(Long id); 
-    void deleteCliente(Long id);    
+
+    Optional<Clientes> getClientesById(Long id);
+
+    void deleteCliente(Long id);
 }
