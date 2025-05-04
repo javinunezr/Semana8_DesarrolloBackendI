806deb830002381cd6598980e96e0699b3b4d7ef
LeoOlivaresD <cspraword@gmail.com>
2025-05-04 16:14:43 -0400

    Se modifica crear producto en restcontroller

--- src/main/java/com/duoc/RopaStoreValidation/controllers/RestControllerProductos.java
+++ src/main/java/com/duoc/RopaStoreValidation/controllers/RestControllerProductos.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,34 +20,32 @@ import com.duoc.RopaStoreValidation.services.ProductosService;
 @RequestMapping("/productos")
 @CrossOrigin(origins = "*")
 
-
 public class RestControllerProductos {
     @Autowired
     private ProductosService productosService;
 
     @GetMapping
-    public List<Productos> getAllProductos(){
+    public List<Productos> getAllProductos() {
         return productosService.getAllProductos();
     }
 
     @PostMapping
-    public Productos createProducto(@RequestBody Productos productos) {
-        return productosService.createProducto(productos);
+    public void createProducto(@RequestBody Productos productos) {
+        productosService.createProducto(productos);
     }
-    
+
     @PutMapping("/{id}")
     public Productos updateProducto(@PathVariable Long id, @RequestBody Productos productos) {
         return productosService.updateProducto(id, productos);
     }
-        
+
     @GetMapping("/{id}")
     public Optional<Productos> getProductoById(@PathVariable Long id) {
         return productosService.getProductoById(id);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteProducto(@PathVariable Long id){
+    public void deleteProducto(@PathVariable Long id) {
         productosService.deleteProducto(id);
-    }    
+    }
 }
-
