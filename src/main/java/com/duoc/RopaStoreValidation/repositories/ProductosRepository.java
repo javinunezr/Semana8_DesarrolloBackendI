44bcdc581be33fa1e999028edcf27848529277b4
LeoOlivaresD <cspraword@gmail.com>
2025-05-05 19:08:17 -0400

    creacion de metodo en repository de preoductos para poder buscar por id de sucursal

--- src/main/java/com/duoc/RopaStoreValidation/repositories/ProductosRepository.java
+++ src/main/java/com/duoc/RopaStoreValidation/repositories/ProductosRepository.java
@@ -1,5 +1,7 @@
 package com.duoc.RopaStoreValidation.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.duoc.RopaStoreValidation.models.Productos;
 
 @Repository
 public interface ProductosRepository extends JpaRepository<Productos, Long> {
-
+    List<Productos> findBySucursalId(Long sucursalId);
 }
