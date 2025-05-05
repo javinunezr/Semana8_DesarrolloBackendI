6921053e740328a2e45f3427e333e19e396a57fa
LeoOlivaresD <cspraword@gmail.com>
2025-05-04 22:25:30 -0400

    Se crea metodo en repository para listar todos los pedidos de un cliente segun su id

--- src/main/java/com/duoc/RopaStoreValidation/repositories/PedidosRepository.java
+++ src/main/java/com/duoc/RopaStoreValidation/repositories/PedidosRepository.java
@@ -1,5 +1,7 @@
 package com.duoc.RopaStoreValidation.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.duoc.RopaStoreValidation.models.Pedidos;
 
 @Repository
 public interface PedidosRepository extends JpaRepository<Pedidos, Long> {
-
+    // Metodo que traera todos los pedidos de un cliente segun su id
+    List<Pedidos> findByIdCliente(Long idCliente);
 }
